84be8e76e6451ff3aca269d50cbcbead8611ac15
sergeyamator <sramator@gmail.com>
2024-10-10 14:46:18 +0200

    Added initial Spring Boot

---
+++ src/main/java/com/rubeticket/app/Application.java
@@ -0,0 +1,17 @@
+package com.rubeticket.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	@GetMapping("/status")
+	public String getStatus() {
+		return "OK";
+	}
+}
